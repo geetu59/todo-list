523c39b9a9f1b10cef1c5f99c774a52055af1f6c
geetu59 <37655908+geetu59@users.noreply.github.com>
2022-01-22 18:11:09 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+import { TodoItemsComponent } from './MyComponents/todo-items/todo-items.component';
+import { AddItemComponent } from './MyComponents/add-item/add-item.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './MyComponents/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemsComponent,
+    AddItemComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
